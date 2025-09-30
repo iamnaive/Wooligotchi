6125477457c4452e3be4197e90fa54374de4cc5d
naive <36657473+iamnaive@users.noreply.github.com>
2025-10-01 09:29:04 +1000

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,4 @@
-// src/main.tsx
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
-
 createRoot(document.getElementById("root")!).render(React.createElement(App));
